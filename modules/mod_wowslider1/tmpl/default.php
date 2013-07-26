1f0e2a2d545a545694030240bc660e78757e8f27
alexrah <alexrah@gmail.com>
2013-07-27 01:32:54 +0200

    installed slideshow, removed artisteer slides

--- modules/mod_wowslider1/tmpl/default.php
+++ modules/mod_wowslider1/tmpl/default.php
@@ -11,11 +11,13 @@ defined('_JEXEC') or die('Restricted access');
 <div id="wowslider-container1">
 <div class="ws_images"><ul>
 <li><img src="modules/mod_wowslider1/data1/images/slideshow1.jpg" alt="slideshow1" title="slideshow1" id="wows1_0"/></li>
+<li><img src="modules/mod_wowslider1/data1/images/487367_524181420935652_2001851996_n.jpg" alt="487367_524181420935652_2001851996_n" title="487367_524181420935652_2001851996_n" id="wows1_1"/></li>
+<li><img src="modules/mod_wowslider1/data1/images/602774_522471287773332_2146991734_n.jpg" alt="602774_522471287773332_2146991734_n" title="602774_522471287773332_2146991734_n" id="wows1_2"/></li>
+<li><img src="modules/mod_wowslider1/data1/images/163379_513197142034080_1908392749_n.jpg" alt="163379_513197142034080_1908392749_n" title="163379_513197142034080_1908392749_n" id="wows1_3"/></li>
+<li><img src="modules/mod_wowslider1/data1/images/270810_577375308949596_757477131_n.jpg" alt="270810_577375308949596_757477131_n" title="270810_577375308949596_757477131_n" id="wows1_4"/></li>
+<li><img src="modules/mod_wowslider1/data1/images/302769_505304629489998_387045306_n.jpg" alt="302769_505304629489998_387045306_n" title="302769_505304629489998_387045306_n" id="wows1_5"/></li>
 </ul></div>
-<div class="ws_bullets"><div>
-<a href="#" title="slideshow1"><img src="modules/mod_wowslider1/data1/tooltips/slideshow1.jpg" alt="slideshow1"/>1</a>
-</div></div>
-<span class="wsl"><a href="http://wowslider.com">Slider Website</a> by WOWSlider.com v4.2</span>
+<span class="wsl"><a href="http://wowslider.com">Carousel CSS</a> by WOWSlider.com v4.2m</span>
 <a href="#" class="ws_frame"></a>
 <div class="ws_shadow"></div>
 </div>
